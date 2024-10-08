565dabb0ef4fddc6101dba4c465b01fd976c3db1
monat96 <42468323+monat96@users.noreply.github.com>
2024-10-09 02:35:50 +0900

    feat: apply cctv jpa audition

--- backend/cctv-service/src/main/java/com/kt/alpca/cctv/model/CCTV.java
+++ backend/cctv-service/src/main/java/com/kt/alpca/cctv/model/CCTV.java
@@ -1,13 +1,15 @@
 package com.kt.alpca.cctv.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "cctv")
@@ -15,12 +17,20 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class CCTV {
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, unique = true)
   private String cctvId;
 
   @Column(nullable = false)
   private String ipAddress;
+
+  @Column(nullable = false)
+  private String hlsAddress;
 
   private double longitude;
 
@@ -30,5 +40,11 @@ public class CCTV {
 
   private String locationAddress;
 
-  private String hlsAddress;
+  @CreatedDate
+  @Column(nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  @Column(nullable=false)
+  private LocalDateTime updatedAt;
 }
