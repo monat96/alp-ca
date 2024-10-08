525d283ec1a9eb33a0c43f680cb502bf434fe1ff
monat96 <42468323+monat96@users.noreply.github.com>
2024-10-09 02:13:14 +0900

    remove: remove unused ping-service application

--- backend/ping-service/src/main/java/com/kt/alpca/ping/enums/Status.java
+++
@@ -1,8 +0,0 @@
-package com.kt.alpca.ping.enums;
-
-public enum Status {
-    SUCCESS,
-    LOSS,
-    TIMEOUT,
-    FAIL
-}
--- backend/ping-service/src/main/java/com/kt/alpca/ping/repository/PingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kt.alpca.ping.repository;
-
-import com.kt.alpca.ping.model.Ping;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface PingRepository extends JpaRepository<Ping, Long> {
-}
