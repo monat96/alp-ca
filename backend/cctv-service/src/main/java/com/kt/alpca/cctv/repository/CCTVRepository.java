58e2932f94589e0769656590073e786979d996bd
monat96 <42468323+monat96@users.noreply.github.com>
2024-10-10 00:06:47 +0900

    refactor: CCTVRepository to use Long as the ID type

--- backend/cctv-service/src/main/java/com/kt/alpca/cctv/repository/CCTVRepository.java
+++ backend/cctv-service/src/main/java/com/kt/alpca/cctv/repository/CCTVRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import com.kt.alpca.cctv.model.CCTV;
 
 @RepositoryRestResource(collectionResourceRel = "cctvs", path = "cctvs")
-public interface CCTVRepository extends JpaRepository<CCTV, String> {
+public interface CCTVRepository extends JpaRepository<CCTV, Long> {
 
 }
