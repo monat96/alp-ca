c8b29b6adbd904ccde4eb67f69b2a38a90074cd7
monat96 <42468323+monat96@users.noreply.github.com>
2024-10-10 15:46:45 +0900

    feat: Add gateway module with initial configuration

---
+++ backend/gateway/src/test/java/com/kt/alpca/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt.alpca.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
