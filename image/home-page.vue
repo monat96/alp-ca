7864790f018cb819430465af640b38c983648dfc
SungWoo Jang <67889714+CastleRain@users.noreply.github.com>
2024-10-10 16:36:15 +0900

    Add files via upload

---
+++ image/home-page.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { CCTVTable, CCTVCardList } from '@/features/cctv';
+import { PingStatusList } from '@/features/ping';
+import { MainHeader } from '@/widgets/layout';
+</script>
+
+<template>
+  <MainHeader />
+  <main class="m-10">
+    <div class="flex flex-col gap-4">
+      <PingStatusList />
+      <CCTVCardList />
+      <CCTVTable />
+    </div>
+  </main>
+</template>
